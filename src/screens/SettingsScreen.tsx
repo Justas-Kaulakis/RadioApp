e35eafff9130f327c5dc600e3dd4f7bf7a37a0ae
Justas-Kaulakis <just.kaulakis@gmail.com>
2022-08-10 15:28:14 +0300

    Created basic Layout and added theme colors

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -2,18 +2,21 @@ import {Button, Text, View} from 'react-native';
 import React, {FC} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../navigation/Routes';
+import Layout from '../containers/Layout';
+import useThemeColors from '../hooks/useThemeColors';
 
 const Settings: FC<NativeStackScreenProps<RootStackParamList>> = ({
   navigation,
 }) => {
+  const colors = useThemeColors();
   return (
-    <View>
-      <Text>SETTINGS</Text>
+    <Layout>
+      <Text style={{color: colors.primary}}>SETTINGS</Text>
       <Button
         title="Go back to main!"
         onPress={() => navigation.navigate('Main')}
       />
-    </View>
+    </Layout>
   );
 };
 
