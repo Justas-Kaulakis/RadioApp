3076136930c172c882f4c776c513547eba8f8b3f
Justas-Kaulakis <just.kaulakis@gmail.com>
2022-08-10 15:25:50 +0300

    #3 Created a way to get custom theme colors

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DarkTheme} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from '../screens/MainScreen';
 import SettingsScreen from '../screens/SettingsScreen';
@@ -13,8 +13,8 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Routes: FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <NavigationContainer theme={DarkTheme}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen
           name="Main"
           component={MainScreen}
